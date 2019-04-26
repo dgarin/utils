5ce35e0cbaf167594d13e20e1dd0a339ebe97eba
Dante Garin <dantegarin@gmail.com>
2019-04-26 16:01:35 -0400

    * CHG: stdout format

--- utilities.go
+++ utilities.go
@@ -11,46 +11,35 @@ func GetIPMAC() (mac string, ip string) {
 	addrs, err := net.InterfaceAddrs()
 
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("ERROR: %s\n", err.Error())
 	}
 
 	var probeIP, currentNetworkHardwareName string
 
 	for _, address := range addrs {
-
-		// check the address type and if it is not a loopback the display it
-		// = GET LOCAL IP ADDRESS
 		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
 			if ipnet.IP.To4() != nil {
-				fmt.Println("Current IP address : ", ipnet.IP.String())
 				probeIP = ipnet.IP.String()
 			}
 		}
 	}
+
 	interfaces, _ := net.Interfaces()
 	for _, interf := range interfaces {
 
 		if addrs, err := interf.Addrs(); err == nil {
-			for index, addr := range addrs {
-				fmt.Println("[", index, "]", interf.Name, ">", addr)
-
-				// only interested in the name with current IP address
+			for _, addr := range addrs {
 				if strings.Contains(addr.String(), probeIP) {
-					fmt.Println("Use name : ", interf.Name)
 					currentNetworkHardwareName = interf.Name
 				}
 			}
 		}
 	}
 
-	fmt.Println("------------------------------")
-
-	// extract the hardware information base on the interface name
-	// capture above
 	netInterface, err := net.InterfaceByName(currentNetworkHardwareName)
 
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("ERROR: %s\n", err.Error())
 	}
 
 	name := netInterface.Name
@@ -59,10 +48,9 @@ func GetIPMAC() (mac string, ip string) {
 	fmt.Println("Hardware name : ", name)
 	fmt.Println("MAC address : ", macAddress)
 
-	// verify if the MAC address can be parsed properly
 	hwAddr, err := net.ParseMAC(macAddress.String())
 	if err != nil {
-		fmt.Println("No able to parse MAC address : ", err)
+		fmt.Printf("ERROR: %s\n", err.Error())
 		os.Exit(-1)
 	}
 
